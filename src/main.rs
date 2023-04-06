11c0a880e2d4f405987ed61bd249121b03710d9c
YuKun Liu <mrxzx.info@gmail.com>
2023-04-06 13:57:06 -0700

    fix: plugin init

--- src/main.rs
+++ src/main.rs
@@ -9,33 +9,43 @@ async fn main() -> anyhow::Result<()> {
 
     set_up_logging();
 
-    let dioxus_config = DioxusConfig::load()
-        .map_err(|e| anyhow!("Failed to load `Dioxus.toml` because: {e}"))?
-        .unwrap_or_else(|| {
-            log::warn!("You appear to be creating a Dioxus project from scratch; we will use the default config");
-            DioxusConfig::default()
-        });
-
-    PluginManager::init(dioxus_config.plugin)
-        .map_err(|e| anyhow!("🚫 Plugin system initialization failed: {e}"))?;
-
     match args.action {
         Translate(opts) => opts
             .translate()
             .map_err(|e| anyhow!("🚫 Translation of HTML into RSX failed: {}", e)),
 
-        Build(opts) => opts
-            .build()
-            .map_err(|e| anyhow!("🚫 Building project failed: {}", e)),
+        Build(opts) => {
+            let dioxus_config = DioxusConfig::load()
+            .map_err(|e| anyhow!("Failed to load `Dioxus.toml` because: {e}"))?
+            .unwrap_or_else(|| {
+                log::warn!("You appear to be creating a Dioxus project from scratch; we will use the default config");
+                DioxusConfig::default()
+            });
+            PluginManager::init(dioxus_config.plugin)
+                .map_err(|e| anyhow!("🚫 Plugin system initialization failed: {e}"))?;
+
+            opts.build()
+                .map_err(|e| anyhow!("🚫 Building project failed: {}", e))
+        }
 
         Clean(opts) => opts
             .clean()
             .map_err(|e| anyhow!("🚫 Cleaning project failed: {}", e)),
 
-        Serve(opts) => opts
-            .serve()
-            .await
-            .map_err(|e| anyhow!("🚫 Serving project failed: {}", e)),
+        Serve(opts) => {
+            let dioxus_config = DioxusConfig::load()
+            .map_err(|e| anyhow!("Failed to load `Dioxus.toml` because: {e}"))?
+            .unwrap_or_else(|| {
+                log::warn!("You appear to be creating a Dioxus project from scratch; we will use the default config");
+                DioxusConfig::default()
+            });
+            PluginManager::init(dioxus_config.plugin)
+                .map_err(|e| anyhow!("🚫 Plugin system initialization failed: {e}"))?;
+
+            opts.serve()
+                .await
+                .map_err(|e| anyhow!("🚫 Serving project failed: {}", e))
+        }
 
         Create(opts) => opts
             .create()
@@ -45,10 +55,21 @@ async fn main() -> anyhow::Result<()> {
             .config()
             .map_err(|e| anyhow!("🚫 Configuring new project failed: {}", e)),
 
-        Plugin(opts) => opts
+        Plugin(opts) => {
+            let dioxus_config = DioxusConfig::load()
+            .map_err(|e| anyhow!("Failed to load `Dioxus.toml` because: {e}"))?
+            .unwrap_or_else(|| {
+                log::warn!("You appear to be creating a Dioxus project from scratch; we will use the default config");
+                DioxusConfig::default()
+            });
+            PluginManager::init(dioxus_config.plugin)
+                .map_err(|e| anyhow!("🚫 Plugin system initialization failed: {e}"))?;
+
+            opts
             .plugin()
             .await
-            .map_err(|e| anyhow!("🚫 Error with plugin: {}", e)),
+            .map_err(|e| anyhow!("🚫 Error with plugin: {}", e))
+        },
 
         Autoformat(opts) => opts
             .autoformat()
