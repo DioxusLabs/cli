26b059abc5ff583ff903420b0909b874e569608f
YuKun Liu <mrxzx.info@gmail.com>
2023-04-18 22:59:31 -0700

    feat: add read_dir api

--- src/plugin/interface/fs.rs
+++ src/plugin/interface/fs.rs
@@ -74,6 +74,19 @@ impl UserData for PluginFileSystem {
             }
             return Ok(true);
         });
+        methods.add_function("read_dir", |_, path: String| {
+            let mut result = Vec::new();
+            let path = PathBuf::from(path);
+            if path.is_dir() {
+                let list = std::fs::read_dir(path).unwrap();
+                for i in list {
+                    if let Ok(i) = i {
+                        result.push(i.path().display().to_string());
+                    }
+                }
+            }
+            Ok(result)
+        });
         methods.add_function("unzip_file", |_, args: (String, String)| {
             let file = PathBuf::from(args.0);
             let target = PathBuf::from(args.1);
